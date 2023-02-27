f00d19a8e9e01dc9aeaedc9dc57d1197ddc3e6a1
Syifaa2706 <114551372+syifaa2706@users.noreply.github.com>
2023-02-27 17:39:51 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nama = 'khoirunnisa syifa fadilah';
+    $umur = 19;
+    $berat = 56;
+
+    echo 'Nama saya : '   .  $nama;
+    echo '<br>Umur saya : '   .  $umur;
+    echo '<br>Berat saya : '  .  $berat;
