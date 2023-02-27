5514fce9d387912e102942e4816474191d8bfcd3
Syifaa2706 <114551372+syifaa2706@users.noreply.github.com>
2023-02-27 17:38:32 +0700

    Create coba1.php

---
+++ coba1.php
@@ -0,0 +1,10 @@
+<?php
+
+    define('PHI' , 3.14 );
+    define('DBNAME',  'inventori');
+    define('DBSERVER', 'localhost');
+
+    echo 'Nilai Phi : ' .PHI;
+    echo 'Nama database nya : '  . DBNAME;
+    echo '<br> Lokasi Database nya : ' .  DBSERVER;
+    
